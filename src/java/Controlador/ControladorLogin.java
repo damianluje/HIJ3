34dc9bacdc98c3697a8004d3ebe3a3d012dfe3f6
Damian <Damian@DESKTOP-U73RJV5>
2016-02-02 18:45:13 -0500

    Add Login MD5

--- src/java/Controlador/ControladorLogin.java
+++ src/java/Controlador/ControladorLogin.java
@@ -11,71 +11,82 @@ package Controlador;
  */
 import Modelo.Usuario;
 import java.io.Serializable;
- 
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
- 
+
 import Servicios.UsuarioFacade;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import javax.ejb.EJB;
- 
+
 @ManagedBean
 @SessionScoped
 public class ControladorLogin implements Serializable {
- 
+
     private static final long serialVersionUID = 1094801825228386363L;
-     
+
     @EJB
     private UsuarioFacade serUsu;
     private String pwd;
     private String msg;
     private String user;
- 
+
     public String getPwd() {
         return pwd;
     }
- 
+
     public void setPwd(String pwd) {
         this.pwd = pwd;
     }
- 
+
     public String getMsg() {
         return msg;
     }
- 
+
     public void setMsg(String msg) {
         this.msg = msg;
     }
- 
+
     public String getUser() {
         return user;
     }
- 
+
     public void setUser(String user) {
         this.user = user;
     }
- 
+
     //validate login
     public String validateUsernamePassword() {
-        
-        ArrayList<Usuario> lista= new ArrayList<>(serUsu.findAll());
-        
-        boolean valid=false;
-        
+
+        ArrayList<Usuario> lista = new ArrayList<>(serUsu.findAll());
+
+        boolean valid = false;
+
         for (int i = 0; i < lista.size(); i++) {
             Usuario usuario = lista.get(i);
-            if (usuario.getUsuId().equals(user) && usuario.getUsuPassword().equals(pwd)) {
-                valid=true;
-                break;
+            try {
+                MessageDigest md = MessageDigest.getInstance("MD5");
+                md.update(pwd.getBytes());
+                byte[] digest = md.digest();
+                StringBuffer sb = new StringBuffer();
+                for (byte b : digest) {
+                    sb.append(String.format("%02x", b & 0xff));
+                }
+                if (usuario.getUsuId().equals(user) && usuario.getUsuPassword().equals(sb.toString())) {
+                    valid = true;
+                    break;
+                }
+            } catch (NoSuchAlgorithmException noSuchAlgorithmException) {
+                System.out.println("Error: MD5");
             }
-            
+
         }
-        
-        
-        
+
         if (valid) {
             HttpSession session = SessionBean.getSession();
             session.setAttribute("username", user);
@@ -89,7 +100,7 @@ public class ControladorLogin implements Serializable {
             return "login";
         }
     }
- 
+
     //logout event, invalidate session
     public String logout() {
         HttpSession session = SessionBean.getSession();
