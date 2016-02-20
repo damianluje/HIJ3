f5541ea8b61d2188a266a2cf64be74a6e9b431b1
Damian <Damian@DESKTOP-U73RJV5>
2016-02-20 15:01:04 -0500

    Validación de cedula, agregación de permisos en el módulo de Salud

---
+++ src/java/Controlador/CedulaValidator.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import java.util.Map;
+import java.util.regex.Pattern;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+import org.primefaces.validate.ClientValidator;
+
+/**
+ * Custom JSF Validator for Email input
+ */
+@FacesValidator("custom.cedulaValidatos")
+public class CedulaValidator implements Validator, ClientValidator {
+
+    private Pattern pattern;
+
+    private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+            + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+    public CedulaValidator() {
+        pattern = Pattern.compile(EMAIL_PATTERN);
+    }
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        if (value == null) {
+            
+        }
+        if (validadorDeCedula(value.toString())==false) {
+            throw new ValidatorException(new FacesMessage(FacesMessage.SEVERITY_ERROR, "Validation Error", 
+                        "No es una cédula válida"));
+           
+        }
+
+    }
+
+    public boolean validadorDeCedula(String cedula) {
+        boolean cedulaCorrecta = false;
+
+        try {
+
+            if (cedula.length() == 10) // ConstantesApp.LongitudCedula
+            {
+                int tercerDigito = Integer.parseInt(cedula.substring(2, 3));
+                if (tercerDigito < 6) {
+// Coeficientes de validación cédula
+// El decimo digito se lo considera dígito verificador
+                    int[] coefValCedula = {2, 1, 2, 1, 2, 1, 2, 1, 2};
+                    int verificador = Integer.parseInt(cedula.substring(9, 10));
+                    int suma = 0;
+                    int digito = 0;
+                    for (int i = 0; i < (cedula.length() - 1); i++) {
+                        digito = Integer.parseInt(cedula.substring(i, i + 1)) * coefValCedula[i];
+                        suma += ((digito % 10) + (digito / 10));
+                    }
+
+                    if ((suma % 10 == 0) && (suma % 10 == verificador)) {
+                        cedulaCorrecta = true;
+                    } else if ((10 - (suma % 10)) == verificador) {
+                        cedulaCorrecta = true;
+                    } else {
+                        cedulaCorrecta = false;
+                    }
+                } else {
+                    cedulaCorrecta = false;
+                }
+            } else {
+                cedulaCorrecta = false;
+            }
+        } catch (NumberFormatException nfe) {
+            cedulaCorrecta = false;
+        } catch (Exception err) {
+            System.out.println("Una excepcion ocurrio en el proceso de validadcion");
+            cedulaCorrecta = false;
+        }
+
+        if (!cedulaCorrecta) {
+            System.out.println("La Cédula ingresada es Incorrecta");
+        }
+        return cedulaCorrecta;
+    }
+
+    public Map<String, Object> getMetadata() {
+        return null;
+    }
+
+    public String getValidatorId() {
+        return "custom.emailValidator";
+    }
+
+}
--- src/java/Controlador/ControladorEmpleado.java
+++ src/java/Controlador/ControladorEmpleado.java
@@ -18,35 +18,41 @@ import javax.faces.context.FacesContext;
 import Servicios.EmpleadoFacade;
 import java.util.AbstractList;
 import javax.faces.event.ValueChangeEvent;
+
 /**
  *
  * @author homer
  */
 @ManagedBean
 @ViewScoped
-public class ControladorEmpleado implements Serializable{
+public class ControladorEmpleado implements Serializable {
+
     @EJB
     private EmpleadoFacade serEmp;
     private Empleado empleado;
     private List<Empleado> empleados;
 
     public ControladorEmpleado() {
-        empleado= new Empleado();
+        empleado = new Empleado();
         empleados = new ArrayList<Empleado>();
     }
+
     @PostConstruct
     public void cargarDatos() {
         empleados = serEmp.findAll();
     }
+
     public void limpiar() {
         empleado = new Empleado();
     }
+
     public void actualizarEmpleado() {
         //System.out.println("*************" + opcion.getCodigoSistema());
         serEmp.edit(empleado);
-        System.out.println("Actualizando Opcion: "+empleado+empleado.getEmpCodigo());
+        System.out.println("Actualizando Opcion: " + empleado + empleado.getEmpCodigo());
         limpiar();
     }
+
     public void eliminarEmpleado() {
         try {
             serEmp.remove(empleado);
@@ -60,19 +66,21 @@ public class ControladorEmpleado implements Serializable{
         limpiar();
 
     }
+
     public void valueChangeMethod(ValueChangeEvent e) {
 
         for (int i = 0; i < empleados.size(); i++) {
             Empleado opc = empleados.get(i);
             if (opc.getEmpCodigo() == Integer.parseInt(e.getNewValue().toString())) {
                 empleado = opc;
-                }
+            }
         }
 
     }
+
     public void ingresarEmpleado() {
         try {
-            System.out.println("Empleado:" + empleado+empleado.getEmpCodigo());
+            System.out.println("Empleado:" + empleado + empleado.getEmpCodigo());
             serEmp.create(empleado);
             this.empleados = serEmp.findAll();
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Empleado Ingresado", ""));
@@ -107,6 +115,51 @@ public class ControladorEmpleado implements Serializable{
     public void setEmpleados(List<Empleado> empleados) {
         this.empleados = empleados;
     }
-    
-    
+
+    public boolean validadorDeCedula(String cedula) {
+        boolean cedulaCorrecta = false;
+
+        try {
+
+            if (cedula.length() == 10) // ConstantesApp.LongitudCedula
+            {
+                int tercerDigito = Integer.parseInt(cedula.substring(2, 3));
+                if (tercerDigito < 6) {
+// Coeficientes de validación cédula
+// El decimo digito se lo considera dígito verificador
+                    int[] coefValCedula = {2, 1, 2, 1, 2, 1, 2, 1, 2};
+                    int verificador = Integer.parseInt(cedula.substring(9, 10));
+                    int suma = 0;
+                    int digito = 0;
+                    for (int i = 0; i < (cedula.length() - 1); i++) {
+                        digito = Integer.parseInt(cedula.substring(i, i + 1)) * coefValCedula[i];
+                        suma += ((digito % 10) + (digito / 10));
+                    }
+
+                    if ((suma % 10 == 0) && (suma % 10 == verificador)) {
+                        cedulaCorrecta = true;
+                    } else if ((10 - (suma % 10)) == verificador) {
+                        cedulaCorrecta = true;
+                    } else {
+                        cedulaCorrecta = false;
+                    }
+                } else {
+                    cedulaCorrecta = false;
+                }
+            } else {
+                cedulaCorrecta = false;
+            }
+        } catch (NumberFormatException nfe) {
+            cedulaCorrecta = false;
+        } catch (Exception err) {
+            System.out.println("Una excepcion ocurrio en el proceso de validadcion");
+            cedulaCorrecta = false;
+        }
+
+        if (!cedulaCorrecta) {
+            System.out.println("La Cédula ingresada es Incorrecta");
+        }
+        return cedulaCorrecta;
+    }
+
 }
